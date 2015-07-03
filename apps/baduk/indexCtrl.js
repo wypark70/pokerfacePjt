724ccd0215ff7d0d298960fc03f5d9eea0ec51bf
wypark70 <wypark70@gmail.com>
2015-07-03 18:13:34 +0900

    물르기 구현

--- apps/baduk/indexCtrl.js
+++ apps/baduk/indexCtrl.js
@@ -2,7 +2,7 @@
  * Created by W.Y.Park on 2015-03-04.
  */
 function indexCtrl($scope) {
-    $scope.appName = '한글테스트';
+    $scope.appName = 'Baduk';
     $scope.menus = [];
     $scope.menus.push({name: '대기실', url: '#/home', class:''});
     $scope.menus.push({name: '대국실', url: '#/deagook', class:''});

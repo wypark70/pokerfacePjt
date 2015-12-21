711b75977eade667c9684ba407f95de01dd5dbcc
W.Y.Park <wypark70@gmail.com>
2015-12-21 17:58:07 +0900

    Puzzle 게임 추가

--- apps/baduk/js/controller/homeCtrl.js
+++ apps/baduk/js/controller/homeCtrl.js
@@ -318,6 +318,32 @@ define([], function () {
                 }
             }
         });
+
+        new jPlayerPlaylist({
+            jPlayer: "#jquery_jplayer_1",
+            cssSelectorAncestor: "#jp_container_1"
+        }, [
+            {
+                title:"윤미래 - 너를 사랑해.mp3",
+                mp3:"../../music/윤미래 - 너를 사랑해.mp3"
+            },
+            {
+                title:"백지영 - 잊지 말아요.mp3",
+                mp3:"../../music/백지영 - 잊지 말아요.mp3"
+            },
+            {
+                title:"백지영 - 총맞은것처럼.mp3",
+                mp3:"../../music/백지영 - 총맞은것처럼.mp3"
+            },
+        ], {
+            swfPath: "../../../libs/jplayer/2.9.2/jplayer",
+            supplied: "oga, mp3",
+            wmode: "window",
+            useStateClassSkin: true,
+            autoBlur: false,
+            smoothPlayBar: true,
+            keyEnabled: true
+        });
     };
 
     homeCtrl.$inject = ['$scope'];
